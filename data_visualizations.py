a2b446cca0c5a56f913b3ae137e18a3db606f11c
kylejurkowski <114253082+kylejurkowski@users.noreply.github.com>
2022-12-06 15:19:17 -0800

    Update data_visualizations.py

--- data_visualizations.py
+++ data_visualizations.py
@@ -4,20 +4,18 @@ import pandas as pd
 
 class data_visualizations:
     
-    def __init__(self, col1, col2=[0]):
+    def __init__(self, col1, col1_name, col2=[0], col2_name='0'):
         self.col1 = col1
         self.col2 = col2
+        self.col1_name = col1_name
+        self.col2_name = col2_name
     
     def compare_variables(self):
         
         
         try:
             fig, ax = plt.subplots(1, figsize=(20,10))
-            '''
-            Need to figure out how to get the title of the column from the colum info itself, so as to
-            limit the amount of parameters the user needs to input when initiating a class.
-            ''' 
-            # ax.set(xlabel = self.var1_title, ylabel = self.var2_title, title = self.var1_title + ' vs. ' + self.var2_title)
+            ax.set(xlabel = self.col1_name, ylabel = self.col2_name, title = self.col1_name + ' vs. ' + self.col2_name)
             ax.scatter(self.col1, self.col2, alpha = 0.4, c="blue")
         except ValueError:
             print("In order to compare two variables about a song, you need to input two variables.")
