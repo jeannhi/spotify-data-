19e05dec792bf890faf399429f57f9509c63dfd4
kylejurkowski <114253082+kylejurkowski@users.noreply.github.com>
2022-12-05 21:07:26 -0800

    Update data_visualizations.py

--- data_visualizations.py
+++ data_visualizations.py
@@ -1,22 +1,23 @@
-fig, ax = plt.subplots(1, 5, figsize = (15, 3), sharey = True)
-colors = ["blue", "yellow", "green", "orange", "purple"]
-axis_index = [0,1,2,3,4]
-index = 0
-for continent, df_continent in gapminder.groupby(['continent']):
+from matplotlib import pyplot as plt
+import random
+import pandas as pd
 
-
-
- # run this code to understand what's happening
- # print(continent, df_continent.shape)
-
-
- for country, df_country in df_continent.groupby(['country']):
- # some plotting code here
-
- ax[axis_index[index]].plot(df_country['year'], df_country['lifeExp'],
- color = colors[index], linewidth=0.25)
- ax[axis_index[index]].title.set_text(continent)
- # run this code to understand what's happening
- # print(country, df_country.shape)
-
- index += 1
+class data_visualizations:
+    
+    def __init__(self, col1, col2=[0]):
+        self.col1 = col1
+        self.col2 = col2
+    
+    def compare_variables(self):
+        
+        
+        try:
+            fig, ax = plt.subplots(1, figsize=(20,10))
+            '''
+            Need to figure out how to get the title of the column from the colum info itself, so as to
+            limit the amount of parameters the user needs to input when initiating a class.
+            ''' 
+            # ax.set(xlabel = self.var1_title, ylabel = self.var2_title, title = self.var1_title + ' vs. ' + self.var2_title)
+            ax.scatter(self.col1, self.col2, alpha = 0.4, c="blue")
+        except ValueError:
+            print("In order to compare two variables about a song, you need to input two variables.")
